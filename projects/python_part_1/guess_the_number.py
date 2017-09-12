1650d904bdce6d33671954bfa90a3f251d34182b
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 14:08:41 -0500

    added guess limit

--- projects/python_part_1/guess_the_number.py
+++ projects/python_part_1/guess_the_number.py
@@ -2,15 +2,21 @@
 
 import random
 number = random.randint(1, 10)
+count = 5
 
-print("I am thinking of a number between 1 and 10.")
+print("I am thinking of a number between 1 and 10.\nYou get {} guesses.".format(count))
 guess = int(input("What's the number? "))
 
 while guess != number:
     if guess < number:
-        print("{} is too low. Try again.".format(guess))
+        count -= 1
+        print("{} is too low. Try again.\nYou have {} guesses left.".format(guess, count))
     if guess > number:
-        print("{} is too high. Try again.".format(guess))
+        count -= 1
+        print("{} is too high. Try again.\nYou have {} guesses left.".format(guess, count))
     guess = int(input("What's the number? "))
-if guess == number:
-    print("You got it! You win!")
+    if guess == number:
+        print("You got it! You win!")
+    if count == 0:
+        break
+        print("You ran out of guesses!")
