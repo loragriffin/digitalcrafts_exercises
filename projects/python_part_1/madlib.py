96bf0938d6526416ed18451d26006e260fc650e2
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 10:46:05 -0500

    added madlib exercise

---
+++ projects/python_part_1/madlib.py
@@ -0,0 +1,6 @@
+print("Please fill in the blanks below:\n__(your name)__'s favorite color is __(color)__.")
+name = input("What's your name? ")
+name = name.capitalize()
+color = input("What is your favorite color? ")
+
+print("{}'s favorite color is {}." .format(name, color))
