5a07b89f9d807ef126310def16c03194395479f0
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 13:59:01 -0500

    updated guess the number

--- projects/python_part_1/guess_the_number.py
+++ projects/python_part_1/guess_the_number.py
@@ -5,7 +5,10 @@ print("I am thinking of a number between 1 and 10.")
 guess = int(input("What's the number? "))
 
 while guess != 5:
-    print("Nope, try again.")
+    if guess < 5:
+        print("{} is too low. Try again.".format(guess))
+    if guess > 5:
+        print("{} is too high. Try again.".format(guess))
     guess = int(input("What's the number? "))
 if guess == 5:
     print("You got it! You win!")
