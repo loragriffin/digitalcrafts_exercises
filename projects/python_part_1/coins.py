26d07da73991898256533f1f0cfc482a562cc873
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 13:54:56 -0500

    added exercise 10 - how many coins?

---
+++ projects/python_part_1/coins.py
@@ -0,0 +1,14 @@
+#How many coins?
+
+coins = 0
+print("You have {} coins.".format(coins))
+another = input("Do you want another coin? (yes/no) ")
+another = another.lower()
+
+while another == 'yes':
+    coins += 1
+    print("You have {} coins.".format(coins))
+    another = input("Do you want another coin? (yes/no) ")
+    another = another.lower()
+
+print("Bye!")
