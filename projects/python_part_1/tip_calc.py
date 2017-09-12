31ea6071ce33d767d9864d559b2b80a075ecb94f
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 12:13:36 -0500

    modified formatting

--- projects/python_part_1/tip_calc.py
+++ projects/python_part_1/tip_calc.py
@@ -1,21 +1,18 @@
 #tip calculator
 
-bill = int(input("Total bill amount: "))
+bill = float(input("Total bill amount: "))
 service = input("Level of service (good, bad, or fair): ")
 service = service.lower()
 
 if service == "good":
     tip = bill * .20
     total = bill + tip
-    print("Tip amount: ", tip)
-    print("Total amount: ", total)
+    print("Tip amount: {:.2f}\nTotal amount: {:.2f}".format(tip, total))
 elif service == "bad":
     tip = bill * .10
     total = bill + tip
-    print("Tip amount: ", tip)
-    print("Total amount: ", total)
+    print("Tip amount: {:.2f}\nTotal amount: {:.2f}".format(tip, total))
 elif service == "fair":
     tip = bill * .15
     total = bill + tip
-    print("Tip amount: ", tip)
-    print("Total amount: ", total)
+    print("Tip amount: {:.2f}\nTotal amount: {:.2f}".format(tip, total))
