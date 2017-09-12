a64004a2b19f55cc66292bb2233b2212835eaf4a
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-12 14:01:27 -0500

    added random number generator

--- projects/python_part_1/guess_the_number.py
+++ projects/python_part_1/guess_the_number.py
@@ -1,14 +1,16 @@
 # guess the number
 
-number = 5
+import random
+number = random.randint(1, 10)
+
 print("I am thinking of a number between 1 and 10.")
 guess = int(input("What's the number? "))
 
-while guess != 5:
-    if guess < 5:
+while guess != number:
+    if guess < number:
         print("{} is too low. Try again.".format(guess))
-    if guess > 5:
+    if guess > number:
         print("{} is too high. Try again.".format(guess))
     guess = int(input("What's the number? "))
-if guess == 5:
+if guess == number:
     print("You got it! You win!")
