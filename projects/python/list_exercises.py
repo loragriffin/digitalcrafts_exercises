c53ebce70b14cc0830a796bb891ee410ad7d8692
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-13 11:22:15 -0500

    added matrix addition exercise

--- projects/python/list_exercises.py
+++ projects/python/list_exercises.py
@@ -41,8 +41,13 @@ for x in range(0, len(numbers)):
 print(multiply)
 
 #9 - Matrix Addition
-matrix = [[2, -2], [5, 3]]
+matrix1 = [[2, -2], [5, 3]]
+matrix2 = [[2, 3], [-1, 0]]
 result = [[0, 0], [0, 0]]
 
-for i in range(len(matrix)):
-    
+for i in range(len(matrix1)):
+    for j in range(len(matrix1[0])):
+        result[i][j] = matrix1[i][j] + matrix2[i][j]
+
+for r in result:
+    print(r)
