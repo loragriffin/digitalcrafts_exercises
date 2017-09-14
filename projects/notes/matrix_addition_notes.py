ad8edf20e52102eda22541bf8b4d609dd76378c2
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-14 09:27:56 -0500

    added new notes

---
+++ projects/notes/matrix_addition_notes.py
@@ -0,0 +1,26 @@
+#matrix addition exercise review
+
+m1 = [
+    [1, 3],
+    [2, 4]
+]
+
+m2 = [
+    [5, 2],
+    [1, 0]
+]
+
+answer = []
+
+for i in range(0, len(m1)):
+    row = m1[i]
+    temp = []
+
+    for j in range(0, len(row)):
+        print(i, j) #shows us what the counter looks like
+        ans = m1[i][j] + m2[i][j]
+        temp.append(ans)
+
+    answer.append(temp)
+
+print(answer)
