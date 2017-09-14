f9a6c81b6ff68ad674098e75e5e6f77a2bfc571d
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-14 13:10:53 -0500

    added more loop exercises

--- projects/python/loop_exercises.py
+++ projects/python/loop_exercises.py
@@ -5,6 +5,8 @@ for x in range(1, 11):
     print(x)
 
 #2 - n to m
+print('--------\nDefine your number range:')
+
 x = int(input("Starting #: "))
 y = int(input("Ending #: "))
 
@@ -12,6 +14,8 @@ for n in range(x, (y + 1)):
     print(n)
 
 #3 - odd numbers
+print('--------\nOnly the odd numbers in your range')
+
 x = int(input("Starting #: "))
 y = int(input("Ending #: "))
 
@@ -20,3 +24,26 @@ for n in range(x, (y + 1)):
         print(n)
 
 #4 - print a square
+print('--------\n5 x 5 square')
+
+def stars():
+    line = '*****'
+    print(line)
+
+stars()
+stars()
+stars()
+stars()
+stars()
+
+#5 - print a square II
+print('--------\nMake your own square')
+
+star = int(input("How large would you like your square? "))
+
+def stars2():
+    line = '*' * star
+    print(line)
+
+for x in range(star):
+    stars2()
