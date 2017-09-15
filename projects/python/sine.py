1a5f5b8206126836fe0b3aff16816c1a43ef18ee
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-15 15:17:33 -0500

    added sine exercise

--- projects/python/sine.py
+++ projects/python/sine.py
@@ -1,2 +1,14 @@
 #function exercises
 #5 - sine
+from math import sin
+import matplotlib.pyplot as plot
+from numpy import arange
+
+x = list(arange(-5, 5, 0.1))
+y = []
+
+for n in x:
+    y.append(sin(n))
+
+plot.plot(x, y)
+plot.show()
