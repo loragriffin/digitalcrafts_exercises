4737a43ac19786daef378fa7a0a873bb524c5ae1
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-19 09:56:06 -0500

    added file i/o exercises

--- projects/python/file_io.py
+++ projects/python/file_io.py
@@ -1,4 +1,27 @@
 #file i/o exercises
 
-#1
-fh = input("File name to open: ")
+#1 - read file
+# user_file = input("File name to open: ")
+#
+def read_file(x):
+    fh = open(x, 'r')
+    content = fh.read()
+    fh.close()
+    print(content)
+
+# read_file(user_file)
+
+#2
+user_file = input("File name to write to: ")
+# text = str(input("What do you want to write to this file? "))
+
+def write_file(x):
+    with open(x, 'w') as x:
+        print((input("First line:")), file = x)
+        again = input("Do you want to add another line? (y/n)")
+        while again == 'y':
+            print((input("Next line:")), file = x)
+            again = input("Do you want to add another line? (y/n)")
+
+write_file(user_file)
+read_file(user_file)
