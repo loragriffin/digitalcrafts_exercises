9785b82869ca1ad543fd76bee1d6faf5b7019079
Lora Griffin <Lora@Loras-MBP.localdomain>
2017-09-20 09:59:38 -0500

    updated object exercises

--- projects/python/object-exercises.py
+++ projects/python/object-exercises.py
@@ -3,6 +3,7 @@ class Person:
         self.name = name
         self.email = email
         self.phone = phone
+        self.friends = []
 
     def greet(self, other_person):
         print('Hello {}, I\'m {}!'.format(other_person.name, self.name))
@@ -10,11 +11,26 @@ class Person:
     def print_info(self):
         print(self.name + "'s email: " + self.email + ', ' + self.name + "'s phone: " + self.phone)
 
+    def add_friend(self, other_person):
+        self.friends.append(other_person)
+
+    def num_friends(self):
+        print(self.name + "'s friends:", len(self.friends))
+
 sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
 jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
 
 sonny.greet(jordan)
 jordan.greet(sonny)
+
+# jordan.friends.append(sonny)
+jordan.add_friend(sonny)
+sonny.add_friend(jordan)
+# print("Jordan's friends:", len(jordan.friends))
+# print("Sonny's friends:", len(sonny.friends))
+
+jordan.num_friends()
+sonny.num_friends()
 
 # print("Sonny's email: ", sonny.email, "\nSonny's phone: ", sonny.phone)
 # print("Jordan's email: ", jordan.email, "\nJordan's phone: ", jordan.phone)
