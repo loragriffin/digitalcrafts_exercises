549b6aba191c5274bd1a1eaca5bf0acec2c70fb2
Lora Griffin <lora.jean@me.com>
2017-09-29 10:20:05 -0500

    restructured file directory for interview challenges

--- projects/interview challenges/algorithm4.py
+++ projects/interview challenges/algorithm4.py
@@ -1,17 +1,19 @@
-from math import exp, sqrt
+from math import sqrt
 primelist = []
+
 
 def prime(i):
     for j in range(2, i):
         if i % j == 0:
             return False
 
-#y = 13195
+
+# y = 13195
 y = 600851475143
 
 for i in range(2, int(sqrt(y))):
     if y % i == 0 and prime(i) != False:
-         primelist.append(i)
+        primelist.append(i)
 
 print(primelist)
 print(primelist[-1])
