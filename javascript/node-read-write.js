d5925d0942aa5652697dafe00239256cf14ca84c
Lora Griffin <lora.jean@me.com>
2017-11-01 14:34:26 -0500

    adding to node.js exercises

--- javascript/node-read-write.js
+++ javascript/node-read-write.js
@@ -1,22 +1,36 @@
 var fs = require('fs');
 var readline = require('readline');
 
-// var fileName = 'file1.txt'
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("Input filename: ", function(fileName) {
-  fs.readFile(fileName, function (error, buffer) {
-    if (error) {
-      console.log(error.message);
-      rl.close();
-      return;
-    }
-    var contents = buffer.toString();
-    var caps = contents.toUpperCase();
-    console.log('File data in caps: ', caps);
+rl.question("Input filename: ", function(inputFile) {
+  rl.question('Ouput filename: ', function(outputFile) {
+    rl.close();
+    fs.readFile(inputFile, function (error, buffer) {
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+      var contents = buffer.toString();
+      var caps = contents.toUpperCase();
+      fs.writeFile(outputFile, caps, function(error) {
+        if (error) {
+          console.log(error.message);
+          return;
+        }
+        console.log('New file saved: ', outputFile);
+        fs.readFile(outputFile, function (error, buffer) {
+          if (error) {
+            console.log(error.message);
+            return;
+          }
+          var contents = buffer.toString();
+          console.log('Contents: ', buffer.toString());
+        });
+      });
+    });
   });
-  rl.close();
 });
